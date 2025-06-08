05d9c5dc67b40ab18d4711378daf290481421765
lowresboots <lowresboots@gmail.com>
2025-06-08 15:20:55 -0400

    first commit - initial structure

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles/main.css';
+
+console.log('AM Portfolio loaded successfully');
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets/images', to: 'images' }
+      ],
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+};
